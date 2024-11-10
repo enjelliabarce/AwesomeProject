f32f30091b3312580e29111a9d0002393ffdba77
enjelliabarce <enjelliabarcepratama@mail.ugm.ac.id>
2024-11-10 21:16:50 +0700

    pgpbl_10

--- BottomNav.js
+++ BottomNav.js
@@ -2,20 +2,31 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
+import Profile from './App';
+import Mahasiswa from './Mahasiswa';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import {faUser} from '@fortawesome/free-solid-svg-icons';
+import { faUserGraduate } from '@fortawesome/free-solid-svg-icons';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { WebView } from 'react-native-webview';
 function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Home!</Text>
-    </View>
+    <Profile />
   );
 }
 
-function SettingsScreen() {
+function DataMahasiswaScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Settings!</Text>
-    </View>
+    <Mahasiswa />
+  );
+}
+
+function WebScreen() {
+  return (
+    <WebView
+        source={{ uri: 'https://github.com/enjelliabarce' }}
+        
+      />
   );
 }
 
@@ -25,8 +36,21 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name="Home" component={HomeScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+        <Tab.Screen name="Profile" component={HomeScreen} 
+      options={{ headerShown: false,
+        tabBarIcon: ({ color }) => (
+          <FontAwesomeIcon icon={faUser} color={color} size={20} />
+        ),}}/>
+        <Tab.Screen name="Mahasiswa" component={DataMahasiswaScreen}  options={{ headerShown: true,
+        tabBarIcon: ({ color }) => (
+          <FontAwesomeIcon icon={faUserGraduate} color={color} size={20} />
+        ),}}/>
+
+      <Tab.Screen name="Github" component={WebScreen}  options={{ 
+        tabBarIcon: ({ color }) => (
+          <FontAwesomeIcon icon={faGithub} color={color} size={20} />
+        ),}}/>
+
       </Tab.Navigator>
     </NavigationContainer>
   );
